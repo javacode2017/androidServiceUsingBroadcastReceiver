070089d3748920d221ec4a3062ae6d5f97dd24a2
sujith <sujithreddy440@gmail.com>
2020-06-08 12:07:38 +0530

    updating activity from service using BroadcastReceiver

---
+++ app/src/main/java/com/example/android/concurrency/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.android.concurrency;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.example.android.concurrency.services.MyDownloadService;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MyTag";
+    public static final String MESSAGE_KEY = "message_key";
+    private ScrollView mScroll;
+    private TextView mLog;
+    private ProgressBar mProgressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    public void runCode(View v) {
+        log("Running code");
+        displayProgressBar(true);
+
+        //send intent to download service
+
+        for (String song:Playlist.songs){
+            Intent intent=new Intent(MainActivity.this,MyDownloadService.class);
+            intent.putExtra(MESSAGE_KEY,song);
+
+            startService(intent);
+        }
+
+    }
+
+    private void initViews() {
+        mScroll = (ScrollView) findViewById(R.id.scrollLog);
+        mLog = (TextView) findViewById(R.id.tvLog);
+        mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
+    }
+
+    public void clearOutput(View v) {
+        mLog.setText("");
+        scrollTextToEnd();
+    }
+
+    public void log(String message) {
+        Log.i(TAG, message);
+        mLog.append(message + "\n");
+        scrollTextToEnd();
+    }
+
+    private void scrollTextToEnd() {
+        mScroll.post(new Runnable() {
+            @Override
+            public void run() {
+                mScroll.fullScroll(ScrollView.FOCUS_DOWN);
+            }
+        });
+    }
+
+    public void displayProgressBar(boolean display) {
+        if (display) {
+            mProgressBar.setVisibility(View.VISIBLE);
+        } else {
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+}
